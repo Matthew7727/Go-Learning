e9ed1f91740f9a69b92d08e30acb8f257424082a
Matthew7727 <matthew.eccleston17@gmail.com>
2024-04-14 15:20:34 +0100

    ?

---
+++ go-essentials/profit-calculator-functions copy/profit-calculator.go
@@ -0,0 +1,47 @@
+
+// Profit Calculator
+// CLI Tool in Go
+// Ask User for Renvue, Expenses and Tax Rate
+// Calualates Earning Before Tax and Earings After Tax and ratio of the two
+
+package main
+
+import (
+	"fmt"
+)
+
+const taxRate float64 = 30
+
+func main() {
+
+	revenue := getUserInput("Please input your Revenue: ")
+	expenses := getUserInput("Please input your Total Expenses: ")
+
+	preTaxProfit, postTaxProfit := calculateProfits(revenue, expenses)
+
+	ratio := calcuateRatio(preTaxProfit, postTaxProfit)
+
+	fmt.Printf("Yor pre tax revenue is: £%.2f \n ", preTaxProfit)
+	fmt.Printf("Your post tax revenue is: £%.2f \n ", postTaxProfit)
+	fmt.Printf("The Ratio is: %.2f \n", ratio)
+
+}
+
+func getUserInput(text string) (input float64) {
+	fmt.Print(text)
+	fmt.Scan(&input)
+	return
+}
+
+func calculateProfits(revenue float64, expenses float64) (float64, float64) {
+	preTaxRevenue := revenue - expenses
+	var taxFloat float64 = 1 - taxRate / 100
+
+	postTaxRevenue := preTaxRevenue * taxFloat
+
+	return preTaxRevenue, postTaxRevenue
+}
+
+func calcuateRatio(preTax, postTax float64) (float64) {
+	return preTax / postTax
+}
