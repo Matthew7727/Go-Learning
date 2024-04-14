99858f45c161059f26346e23fe75e75048116a16
Matthew7727 <matthew.eccleston17@gmail.com>
2024-04-14 13:26:13 +0100

    done challenge

--- go-essentials/profit-calculator-functions-validation-file-read-write/profit-calculator.go
+++ go-essentials/profit-calculator-functions-validation-file-read-write/profit-calculator.go
@@ -1,26 +1,33 @@
-
 // Profit Calculator
 // CLI Tool in Go
 // Ask User for Renvue, Expenses and Tax Rate
 // Calualates Earning Before Tax and Earings After Tax and ratio of the two
 // Validates User Input
 // - No Negative Numbers
-// - Not 0 
+// - Not 0
 // Stores Resutls into a file
 
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 const taxRate float64 = 30
 
 func main() {
 
-	revenue := getUserInput("Please input your Revenue: ")
-	expenses := getUserInput("Please input your Total Expenses: ")
+	revenue, err := getUserInput("Please input your Revenue: ")
+	if err != nil {
+		panic("Error with revenue input")
+	}
 
+	expenses, err := getUserInput("Please input your Total Expenses: ")
+	if err != nil {
+		panic("Error with expenese input")
+	}
 	preTaxProfit, postTaxProfit := calculateProfits(revenue, expenses)
 
 	ratio := calcuateRatio(preTaxProfit, postTaxProfit)
@@ -29,23 +36,37 @@ func main() {
 	fmt.Printf("Your post tax revenue is: £%.2f \n ", postTaxProfit)
 	fmt.Printf("The Ratio is: %.2f \n", ratio)
 
+	saveToFile(preTaxProfit, postTaxProfit, ratio)
+
 }
 
-func getUserInput(text string) (input float64) {
+func saveToFile(preTax float64, postTax float64, ratio float64) {
+	stringToWrite := fmt.Sprintf("The Pre Tax Proft was: £%.2f. The Post Tax Proft was: £%.2f with a ratio of %.2f", preTax, postTax, ratio)
+	os.WriteFile("Profts.txt", []byte(stringToWrite), 0644)
+}
+
+func getUserInput(text string) (float64, error) {
+	var input float64
+
 	fmt.Print(text)
 	fmt.Scan(&input)
-	return
+
+	if input <= 0 {
+		return 0, errors.New("you have entered an invalid amount")
+	} else {
+		return input, nil
+	}
 }
 
 func calculateProfits(revenue float64, expenses float64) (float64, float64) {
 	preTaxRevenue := revenue - expenses
-	var taxFloat float64 = 1 - taxRate / 100
+	var taxFloat float64 = 1 - taxRate/100
 
 	postTaxRevenue := preTaxRevenue * taxFloat
 
 	return preTaxRevenue, postTaxRevenue
 }
 
-func calcuateRatio(preTax, postTax float64) (float64) {
+func calcuateRatio(preTax, postTax float64) float64 {
 	return preTax / postTax
 }
