df9f957003216eb9f1e777e6d8c1e443cb7ce7ec
Matthew7727 <matthew.eccleston17@gmail.com>
2024-04-25 20:46:27 +0100

    added struct

--- structs-and-custom-types/structs/structs.go
+++ structs-and-custom-types/structs/structs.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type user struct {
+	firstName string
+	lastName string
+	birthDate string
+	createdAt time.Time
+}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	// ... do something awesome with that gathered data!
+	
 
 	fmt.Println(firstName, lastName, birthdate)
 }
