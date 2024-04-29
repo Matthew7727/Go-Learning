ae4dd9457a21c667664c9c2aa197e4c9a2c6130f
Eccleston <matthew.eccleston17@gmail.com>
2024-04-29 19:58:56 +0100

    basic arrays and slices

---
+++ collections/lists/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var productName [4]string 
+	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
+	productName[3] = "Hello Darkness "
+
+	fmt.Println(prices)
+	fmt.Println(productName)
+
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
+	fmt.Println(len(productName), cap(featuredPrices))
+
+}
+
