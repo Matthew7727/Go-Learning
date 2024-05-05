52add3358eee784e79871534dce718dd8418bca2
Matthew7727 <matthew.eccleston17@gmail.com>
2024-05-05 20:51:14 +0100

    recursion

---
+++ advanced functions/anonymous-functions/go.mod
@@ -0,0 +1,3 @@
+module example.com/anonymous-func
+
+go 1.22.2
---
+++ advanced functions/recursion/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func main() {
+
+	fact := factorial(5)
+	fmt.Println(fact)
+
+}
+
+func factorial(number int) int {
+	if (number == 0) {
+		return 1
+	} else {
+		return number * factorial(number - 1)
+	}
+	
+}
