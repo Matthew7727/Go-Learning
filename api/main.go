b1bcef6d61fbf955da118173a454344b9a32869f
Matthew7727 <matthew.eccleston17@gmail.com>
2025-03-11 13:42:29 +0000

    api - db - linked

--- api/main.go
+++ api/main.go
@@ -18,7 +18,11 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetEvents()
+	events, err := models.GetEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "data silly"})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -32,6 +36,10 @@ func createEvent(context *gin.Context) {
 
 	event.ID = 1 
 	event.UserID = 1
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "event fell over"})
+		return
+	}
 	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
