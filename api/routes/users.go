b0769751ef4ab791e376432e79b633c7edaa8b5d
Matthew7727 <matthew.eccleston17@gmail.com>
2025-03-13 08:16:28 +0000

    Nearly done !

--- api/routes/users.go
+++ api/routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -28,3 +29,29 @@ func signup(context *gin.Context) {
 
 }
 
+func login(context *gin.Context) {
+
+	var user models.User 
+	err := context.ShouldBindBodyWithJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse the request"})
+		return
+	}
+
+	err = user.ValidateLogin()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "couldnt auth user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "logged in", "token": token})
+}
